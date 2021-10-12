3c5776eda544d3eb32e0e3c4aef68bfeb76f966f
tuong404 <tuongka321@gmail.com>
2021-10-12 16:08:03 +0700

    Movie Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,27 @@ modal.onclick = function() {
 }
 
 
-// window.onclick = function(e) {
-//     tabLogin.style.transform = 'translateX(100%)';
-//     tabLogin.style.opacity = '0';
-// }
+// Tabs sign in/ sign up
+
+var btnLogin = document.querySelector('.login_top-signin');
+var btnRegister = document.querySelector('.login_top-signup');
+
+var boxLogin = document.querySelector('.login__bottom');
+var boxRegister = document.querySelector('.register__bottom');
+btnLogin.onclick = function() {
+    btnRegister.classList.remove('active')
+    this.classList.add('active');
+    boxRegister.style.display = 'none';
+    boxLogin.style.display = 'block';
+}
+btnRegister.onclick = function() {
+    btnLogin.classList.remove('active')
+    this.classList.add('active');
+    boxRegister.style.display = 'block';
+    boxLogin.style.display = 'none';
+}
+
+
+function checked() {
+    document.querySelector('input[name="girl"]').checked = true;
+}
